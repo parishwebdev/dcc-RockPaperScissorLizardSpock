776356151b37c01e37987cf203ee29e8cf1d4cbe
parishwebdev <parishwebdev@gmail.com>
2018-05-08 20:16:03 -0500

    added inital classes

---
+++ DCC-RockPaperScissorLizardSpock/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCC_RockPaperScissorLizardSpock
+{
+    class Player
+    {
+        //base class
+    }
+}
